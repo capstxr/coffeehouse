376eb19855ddf572343343e0738956f2ffe00af6
capstxr <114504520+capstxr@users.noreply.github.com>
2024-03-15 13:27:47 +0200

    Mobile nav fix

--- src/ts/components/header/index.tsx
+++ src/ts/components/header/index.tsx
@@ -2,11 +2,12 @@ import { useState, useEffect } from 'react';
 
 import { PiListBold } from "react-icons/pi";
 
+import { slide as Menu } from 'react-burger-menu';
+
 import './header.scss';
 
 const Header = () => {
     const [ hasBg, setHasBg ] = useState<string>('');
-    const [ isOpen, setIsOpen ] = useState<boolean>(false);
 
     window.addEventListener('scroll', function() {
         if (window.scrollY > 75) {
@@ -16,18 +17,10 @@ const Header = () => {
         }
     });
 
-    const handleHamburgerClick = () => {
-        setIsOpen(!isOpen);
-    }
-
-    useEffect(() => {
-        document.body.classList.toggle('is-open');
-    }, [isOpen]);
-
     return (
         <header
             id="header"
-            className={`wrapper ${hasBg} ${isOpen ? 'open': ''}`}
+            className={`wrapper ${hasBg}`}
         >
             <nav className="content" id='nav'>
                 <div className="nav__list">
@@ -91,79 +84,56 @@ const Header = () => {
                         Sign Up
                     </a>
                 </div>
-            </nav>
 
-            <nav id="mobile__nav" className="content">
-                <div className="nav__list">
-                    <a
+                <Menu isOpen={ false } noOverlay right>
+                <a
                         href="#"
-                        className='nav__logo cursive nav__link'
+                        className='nav__link'
                         rel='noreferrer noopener nofollow'
                     >
-                        Bean Scene
+                        Home
                     </a>
-                </div>
 
-                <div className="mobile__nav__content">
-                    <div className="nav__list">
-                        <a
-                            href="#"
-                            className='nav__link'
-                            rel='noreferrer noopener nofollow'
-                        >
-                            Home
-                        </a>
+                    <a
+                        href="#menu"
+                        className='nav__link'
+                        rel='noreferrer noopener nofollow'
+                    >
+                        Menu
+                    </a>
 
-                        <a
-                            href="#menu"
-                            className='nav__link'
-                            rel='noreferrer noopener nofollow'
-                        >
-                            Menu
-                        </a>
+                    <a
+                        href="#why__us"
+                        className='nav__link'
+                        rel='noreferrer noopener nofollow'
+                    >
+                        About Us
+                    </a>
 
-                        <a
-                            href="#why__us"
-                            className='nav__link'
-                            rel='noreferrer noopener nofollow'
-                        >
-                            About Us
-                        </a>
+                    <a
+                        href="#newsletter"
+                        className='nav__link'
+                        rel='noreferrer noopener nofollow'
+                    >
+                        Contact Us
+                    </a>
 
-                        <a
-                            href="#newsletter"
-                            className='nav__link'
-                            rel='noreferrer noopener nofollow'
-                        >
-                            Contact Us
-                        </a>
-                    </div>
+                    <a
+                        href="#"
+                        className='nav__link'
+                        rel='noreferrer noopener nofollow'
+                    >
+                        Sign In
+                    </a>
 
-                    <div className="nav__list">
-                        <a
-                            href="#"
-                            className='nav__link'
-                            rel='noreferrer noopener nofollow'
-                        >
-                            Sign In
-                        </a>
-
-                        <a
-                            href="#"
-                            className='nav__link sign__up__btn'
-                            rel='noreferrer noopener nofollow'
-                        >
-                            Sign Up
-                        </a>
-                    </div>
-                </div>
-
-                <button
-                    className="hamburger"
-                    onClick={handleHamburgerClick}
-                >
-                    <PiListBold/>
-                </button>
+                    <a
+                        href="#"
+                        className='nav__link sign__up__btn'
+                        rel='noreferrer noopener nofollow'
+                    >
+                        Sign Up
+                    </a>
+                </Menu>
             </nav>
         </header>
     );
